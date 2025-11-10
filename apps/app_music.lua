bfa9aabaf44df701426a7bde0ff4a04c56f8b84e
SuperMcBrent <brentsagaert@gmail.com>
2025-11-10 15:33:28 +0100

    key spacing

--- apps/app_music.lua
+++ apps/app_music.lua
@@ -2,39 +2,39 @@ local keys = {
     -- left unplayable padding
     { pitch = -1, x = 3,  black = false, playable = false },
 
-    -- playable range 0–24
-    { pitch = 0,  x = 7,  black = false, playable = true }, -- F#
-    { pitch = 1,  x = 9,  black = true,  playable = true }, -- G
-    { pitch = 2,  x = 11, black = false, playable = true }, -- G#
-    { pitch = 3,  x = 13, black = true,  playable = true }, -- A
-    { pitch = 4,  x = 15, black = false, playable = true }, -- A#
-    { pitch = 5,  x = 17, black = true,  playable = true }, -- B
+    -- playable range 0–24 (F# .. F#)
+    { pitch = 0,  x = 5,  black = true,  playable = true }, -- F#
+    { pitch = 1,  x = 7,  black = false, playable = true }, -- G
+    { pitch = 2,  x = 9,  black = true,  playable = true }, -- G#
+    { pitch = 3,  x = 11, black = false, playable = true }, -- A
+    { pitch = 4,  x = 13, black = true,  playable = true }, -- A#
+    { pitch = 5,  x = 15, black = false, playable = true }, -- B
     { pitch = 6,  x = 19, black = false, playable = true }, -- C
     { pitch = 7,  x = 21, black = true,  playable = true }, -- C#
     { pitch = 8,  x = 23, black = false, playable = true }, -- D
     { pitch = 9,  x = 25, black = true,  playable = true }, -- D#
     { pitch = 10, x = 27, black = false, playable = true }, -- E
-    { pitch = 11, x = 29, black = false, playable = true }, -- F
-    { pitch = 12, x = 31, black = true,  playable = true }, -- F#
-    { pitch = 13, x = 33, black = false, playable = true }, -- G
-    { pitch = 14, x = 35, black = true,  playable = true }, -- G#
-    { pitch = 15, x = 37, black = false, playable = true }, -- A
-    { pitch = 16, x = 39, black = true,  playable = true }, -- A#
-    { pitch = 17, x = 41, black = false, playable = true }, -- B
-    { pitch = 18, x = 43, black = false, playable = true }, -- C
-    { pitch = 19, x = 45, black = true,  playable = true }, -- C#
-    { pitch = 20, x = 47, black = false, playable = true }, -- D
-    { pitch = 21, x = 49, black = true,  playable = true }, -- D#
-    { pitch = 22, x = 51, black = false, playable = true }, -- E
-    { pitch = 23, x = 53, black = false, playable = true }, -- F
-    { pitch = 24, x = 55, black = true,  playable = true }, -- F#
+    { pitch = 11, x = 31, black = false, playable = true }, -- F
+    { pitch = 12, x = 33, black = true,  playable = true }, -- F#
+    { pitch = 13, x = 35, black = false, playable = true }, -- G
+    { pitch = 14, x = 37, black = true,  playable = true }, -- G#
+    { pitch = 15, x = 39, black = false, playable = true }, -- A
+    { pitch = 16, x = 41, black = true,  playable = true }, -- A#
+    { pitch = 17, x = 43, black = false, playable = true }, -- B
+    { pitch = 18, x = 47, black = false, playable = true }, -- C
+    { pitch = 19, x = 49, black = true,  playable = true }, -- C#
+    { pitch = 20, x = 51, black = false, playable = true }, -- D
+    { pitch = 21, x = 53, black = true,  playable = true }, -- D#
+    { pitch = 22, x = 55, black = false, playable = true }, -- E
+    { pitch = 23, x = 59, black = false, playable = true }, -- F
+    { pitch = 24, x = 61, black = true,  playable = true }, -- F#
 
     -- right unplayable padding
-    { pitch = 25, x = 57, black = false, playable = false }
+    { pitch = 25, x = 63, black = false, playable = false }
 }
 
-local WHITE_W, WHITE_H = 3, 5
-local BLACK_W, BLACK_H = 3, 2
+local WHITE_W, WHITE_H = 3, 6
+local BLACK_W, BLACK_H = 3, 3
 local START_Y = 4
 
 local function mainView(ctx)

1c8cc5fb734de315edf8f940701ff4ad6d9f24fd
SuperMcBrent <brentsagaert@gmail.com>
2026-03-20 14:15:23 +0100

    grid modifications

--- apps/app_maintenance.lua
+++ apps/app_maintenance.lua
@@ -1,39 +1,40 @@
 local APPID = "maintenance"
 
+local BTN_W = 17
+local BTN_H = 7
+local H_SPACING = 2
+local V_SPACING = 1
+
+local buttonDefs = {
+    { id = "testbutton_1",  name = "Pyrolyse",  color = colors.green, gridX = 1, gridY = 1 },
+    { id = "testbutton_2",  name = "Button 2",  color = colors.green, gridX = 2, gridY = 1 },
+    { id = "testbutton_3",  name = "Button 3",  color = colors.green, gridX = 3, gridY = 1 },
+    { id = "testbutton_4",  name = "Button 4",  color = colors.green, gridX = 4, gridY = 1 },
+    { id = "testbutton_5",  name = "Button 5",  color = colors.green, gridX = 1, gridY = 2 },
+    { id = "testbutton_6",  name = "Button 6",  color = colors.green, gridX = 2, gridY = 2 },
+    { id = "testbutton_7",  name = "Button 7",  color = colors.green, gridX = 3, gridY = 2 },
+    { id = "testbutton_8",  name = "Button 8",  color = colors.green, gridX = 4, gridY = 2 },
+    { id = "testbutton_9",  name = "Button 9",  color = colors.green, gridX = 1, gridY = 3 },
+    { id = "testbutton_10", name = "Button 10", color = colors.green, gridX = 2, gridY = 3 },
+    { id = "testbutton_11", name = "Button 11", color = colors.green, gridX = 3, gridY = 3 },
+    { id = "testbutton_12", name = "Button 12", color = colors.green, gridX = 4, gridY = 3 },
+    { id = "testbutton_13", name = "Button 13", color = colors.green, gridX = 1, gridY = 4 },
+    { id = "testbutton_14", name = "Button 14", color = colors.green, gridX = 2, gridY = 4 },
+    { id = "testbutton_15", name = "Button 15", color = colors.green, gridX = 3, gridY = 4 },
+    { id = "testbutton_16", name = "Button 16", color = colors.green, gridX = 4, gridY = 4 }
+}
+
+local function getButtonX(gridX)
+    return 2 + (gridX - 1) * (BTN_W + H_SPACING)
+end
+
+local function getButtonY(gridY)
+    return 4 + (gridY - 1) * (BTN_H + V_SPACING)
+end
+
+
 local function mainView(ctx)
     local view = "view_main"
-
-    local BTN_W = 13
-    local BTN_H = 5
-    local H_SPACING = 2
-    local V_SPACING = 1
-
-    local buttonDefs = {
-        { id = "testbutton_1",  name = "Pyrolyse",  color = colors.lime, gridX = 1, gridY = 1 },
-        { id = "testbutton_2",  name = "Button 2",  color = colors.lime, gridX = 2, gridY = 1 },
-        { id = "testbutton_3",  name = "Button 3",  color = colors.lime, gridX = 3, gridY = 1 },
-        { id = "testbutton_4",  name = "Button 4",  color = colors.lime, gridX = 4, gridY = 1 },
-        { id = "testbutton_5",  name = "Button 5",  color = colors.lime, gridX = 1, gridY = 2 },
-        { id = "testbutton_6",  name = "Button 6",  color = colors.lime, gridX = 2, gridY = 2 },
-        { id = "testbutton_7",  name = "Button 7",  color = colors.lime, gridX = 3, gridY = 2 },
-        { id = "testbutton_8",  name = "Button 8",  color = colors.lime, gridX = 4, gridY = 2 },
-        { id = "testbutton_9",  name = "Button 9",  color = colors.lime, gridX = 1, gridY = 3 },
-        { id = "testbutton_10", name = "Button 10", color = colors.lime, gridX = 2, gridY = 3 },
-        { id = "testbutton_11", name = "Button 11", color = colors.lime, gridX = 3, gridY = 3 },
-        { id = "testbutton_12", name = "Button 12", color = colors.lime, gridX = 4, gridY = 3 },
-        { id = "testbutton_13", name = "Button 13", color = colors.lime, gridX = 1, gridY = 4 },
-        { id = "testbutton_14", name = "Button 14", color = colors.lime, gridX = 2, gridY = 4 },
-        { id = "testbutton_15", name = "Button 15", color = colors.lime, gridX = 3, gridY = 4 },
-        { id = "testbutton_16", name = "Button 16", color = colors.lime, gridX = 4, gridY = 4 }
-    }
-
-    local function getButtonX(gridX)
-        return 2 + (gridX - 1) * (BTN_W + H_SPACING)
-    end
-
-    local function getButtonY(gridY)
-        return 4 + (gridY - 1) * (BTN_H + V_SPACING)
-    end
 
     return {
         init = function()
@@ -51,7 +52,7 @@ local function mainView(ctx)
                     h = BTN_H,
                     colorOn = btn.color,
                     textOn = btn.name,
-                    textX = x + 2,
+                    textX = x + 4,
                     textY = y + 1
                 })
             end
